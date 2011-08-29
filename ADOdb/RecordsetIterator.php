bbf479b34162f11e2bbda2f78077765e8a35c796
Dario <dario@ewide.eu>
2011-08-29 07:58:11 +0200

    RecordSet Iterator (ADOdb compat)

---
+++ ADOdb/RecordsetIterator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ADOdb;
+
+/**
+ * RecordsetIterator
+ */
+class RecordsetIterator extends \Iterator
+{
+    protected $rs;
+
+    public function __construct(RecordSet $rs)
+    {
+        $this->rs = $rs;
+    }
+
+    public function rewind()
+    {
+        $this->rs->MoveFirst();
+    }
+
+    public function valid()
+    {
+        return !$this->rs->EOF;
+    }
+
+    public function key()
+    {
+        return $this->rs->CurrentRow();
+    }
+
+    public function current()
+    {
+        return $this->rs->fields;
+    }
+
+    public function next()
+    {
+        $this->rs->MoveNext();
+    }
+
+    public function __call($func, $params)
+    {
+        return call_user_func_array(array($this->rs, $func), $params);
+    }
+
+    public function hasMore()
+    {
+        return!$this->rs->EOF;
+    }
+}
