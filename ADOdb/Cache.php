caeeafb9b265b0c186235cc3b0b442b6d4d12b2b
Dario <dario@ewide.eu>
2011-09-06 15:43:58 +0200

    Added cache factory

--- ADOdb/Cache.php
+++ ADOdb/Cache.php
@@ -6,6 +6,16 @@ use ADOdb\Drivers\Cache as DriverCache;
 
 abstract class Cache implements DriverCache
 {
+    const TYPE_FILE = 1;
+
+    public static function createCache($type = self::TYPE_FILE)
+    {
+        switch ($type) {
+            case self::TYPE_FILE:
+            default:
+                return new Drivers\Cache\File();
+    }
+
     abstract public function read($key, $ttl);
     abstract public function write($key, $value, $ttl);
     abstract public function flush($key);
